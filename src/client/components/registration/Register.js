5014475188c268a9ffbf9309ed87eecd82a20081
Igor Stellmach <istellmach86@gmail.com>
2016-12-06 12:35:54 +0100

    Registrierungsfenster programmiert

---
+++ src/client/components/registration/Register.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Igor Stellmach on 05.12.2016.
+ */
+
+import React from 'react';
+import LabelInput from '../../LabelInput';
+import LabelInputPass from '../../LabelInputPass';
+import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
+
+//Komponente für die Registrierung
+export default class Register extends React.Component
+{
+    render()
+    {
+        // Styling für die Buttons
+        const style = {
+            position: 'relative',
+            textAlign: 'center',
+            display: 'block'
+        };
+
+        // Styling für den Paper
+        const paperStyle = {
+            position: 'relative',
+            marginTop: '50px',
+            marginBottom: '50px',
+            marginLeft: '-150px',
+            left:'50%',
+            display: 'inline-block',
+            borderRadius: '10px', // abgerundete Ecken
+            backgroundColor:'#efefef', // grauer Hintergrund
+            padding: '10px',  // Innenabstand
+        };
+
+        return <div>
+            <MuiThemeProvider>
+                <Paper id="paper" zDepth={5}>
+                <Paper id="paper" zDepth={5} style={paperStyle}>
+                    <h2>Registrieren</h2>
+                    <LabelInput labelTitle="Ihr Name" labelText="Please input your name"></LabelInput>
+                    <LabelInput labelTitle="Email-Adresse" labelText="Please input your e-mail"></LabelInput>
+                    <LabelInputPass labelTitle="Passwort" labelText="Please input your Password"></LabelInputPass>
+                    <LabelInputPass labelTitle="Noch mal Passwort" labelText="Please input your Password again"></LabelInputPass>
+                    <RaisedButton label="Konto erstellen" primary = 'true' style={style}/>
+                    <p style={style}>Sie haben bereits ein Konto?</p>
+                </Paper>
+                </Paper>
+            </MuiThemeProvider>
+        </div>
+    }
+}
