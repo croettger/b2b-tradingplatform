ecd45b7d3b1422e2dc1077fca65738f761f974f6
Igor Stellmach <istellmach86@gmail.com>
2016-12-06 12:34:56 +0100

    Kleine Anpassungen

--- src/client/main.js
+++ src/client/main.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import LabelInput from './LabelInput';
 import Navigation from './Navigation';
+import BottomNavigationTest from './BottomNavigationTest';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import Login from './components/login/Login';
+import Register from './components/registration/Register';
 
 
 //Wird für das Aufklappen von Menüs verwendet z.B in Navigation.js
 injectTapEventPlugin();
 
 //ReactDOM.render(<Welcome name="Christoph"/>, document.getElementById('main'));
-ReactDOM.render(<LabelInput labelTitle="Ein Label:"/>, document.getElementById('eingabe'));
+//ReactDOM.render(<LabelInput labelTitle="Ein Label:"/>, document.getElementById('eingabe'));
 ReactDOM.render(<Navigation title="B2B-TradingPlatform"/>, document.getElementById('header'));
 ReactDOM.render(<Login/>, document.getElementById('anmelden'));
+//ReactDOM.render(<Register/>, document.getElementById('anmelden'));
+ReactDOM.render(<BottomNavigationTest/>, document.getElementById('footer'));
