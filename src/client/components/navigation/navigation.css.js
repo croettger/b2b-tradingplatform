897459eeacb523a4398cffc379b7863236ddbffa
Chris <christh@gmx.net>
2016-12-08 11:51:39 +0100

    CSS-Änderungen hinzugefügt

---
+++ src/client/components/navigation/navigation.css.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Christoph on 08.12.2016.
+ */
+
+
+export default {
+    firstElement: {marginLeft: '20px'},
+    lastElement: {marginRight: '20px'},
+    titleColor: {color: 'white'}
+}
