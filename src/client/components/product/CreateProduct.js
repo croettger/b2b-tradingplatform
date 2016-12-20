81a2aa0d0cd4563e146e701a8de3cb77b890a783
Igor Stellmach <istellmach86@gmail.com>
2016-12-20 19:51:34 +0100

    Create Produck aktualisiert

--- src/client/components/product/CreateProduct.js
+++ src/client/components/product/CreateProduct.js
@@ -11,6 +11,25 @@ import ProductPictures from './ProductPictures';
 
 //Komponente: CreateProduct
 export default class CreateProduct extends React.Component {
+    constructor() {
+        super();
+        this.state = {zeichen: ''} // Zustand --> Anzahl eingegebener Zeichen
+    }
+
+    // Methode liefert die Anzahl der noch verbleibenden Zeichen.
+    getCharsRemaining() {
+        return this.props.maxLength-this.state.zeichen.length;
+    }
+
+    // Methode liefert true wenn die Zeichen noch eingegeben werden können.
+    hasRemainingChars() {
+        return this.getCharsRemaining() >= 0;
+    }
+
+    // Methode verwaltet den Zustand --> Anzahl der Zeichen.
+    charCountChange(event) {
+        this.setState({zeichen: event.target.value});
+    }
 
     render() {
 
@@ -38,19 +57,77 @@ export default class CreateProduct extends React.Component {
             padding: '10px',  // Innenabstand
         };
 
+        /* Array mit den Eigenschaften für Komponente LabelInput*/
+        var dataForLabelInput = [
+            {
+                labelTitle: 'Produktname',
+                labelText: 'Bitte Produktname eingeben',
+                maxLength: '100',
+                fullWidth: 'true'
+            },
+            {
+                labelTitle: 'Artikelnummer',
+                labelText: 'Bitte Artikelnummer eingeben',
+                maxLength: '15',
+                fullWidth: 'true'
+            },
+            {
+                labelTitle: 'Preis',
+                labelText: 'Bitte den Preis eingeben',
+                maxLength: '15',
+                fullWidth: 'true'
+            },
+            {
+                labelTitle: 'Anzahl',
+                labelText: 'Bitte die Anzahl eingeben',
+                maxLength: '10',
+                fullWidth: 'true'
+            },
+            {
+                labelTitle: 'Größe',
+                labelText: 'Bitte die Größe eingeben',
+                maxLength: '10',
+                fullWidth: 'true'
+            },
+            {
+                labelTitle: 'Hersteller',
+                labelText: 'Bitte den Hersteller eingeben',
+                maxLength: '25',
+                fullWidth: 'true'
+            },
+            {
+                labelTitle: 'Kategorie',
+                labelText: 'Bitte Kategorie eingeben',
+                maxLength: '30',
+                fullWidth: 'true'
+            },
+            {
+                labelTitle: 'Produktbeschreibung',
+                inputMultiLine: 'true',
+                rows: '4',
+                maxLength: '1024',
+                fullWidth: 'true'
+            }
+        ];
+
+        /* Iteration in map. Daten werden aus dem Array 'dataForLabelInput' geholt.*/
+
+
         return <div>
             <MuiThemeProvider>
                 <Paper id="paper" zDepth={5} style={paperStyle}>
                     <h2>{this.props.ueberschrift}</h2><br/>
                     <ProductPictures/><br/>
-                    <LabelInput labelTitle="Produktname" labelText="Bitte Produktname eingeben" maxLength = "100" fullWidth = "true"></LabelInput>
-                    <LabelInput labelTitle="Artikelnummer" labelText="Bitte Artikelnummer eingeben" maxLength = "15" fullWidth = "true"></LabelInput>
-                    <LabelInput labelTitle="Preis" labelText="Bitte den Preis eingeben" maxLength = "15" fullWidth = "true"></LabelInput>
-                    <LabelInput labelTitle="Anzahl" labelText="Bitte die Anzahl eingeben" maxLength = "10" fullWidth = "true"></LabelInput>
-                    <LabelInput labelTitle="Größe" labelText="Bitte die Größe eingeben" maxLength = "10" fullWidth = "true"></LabelInput>
-                    <LabelInput labelTitle="Hersteller" labelText="Bitte den Hersteller eingeben" maxLength = "25" fullWidth = "true"></LabelInput>
-                    <LabelInput labelTitle="Kategorie" labelText="Bitte Kategorie eingeben" maxLength = "30" fullWidth = "true"></LabelInput>
-                    <LabelInput labelTitle="Produktbeschreibung" inputMultiLine="true" rows = "4" maxLength = "1024" fullWidth = "true"></LabelInput>
+                    {dataForLabelInput.map((data) => (
+                         <LabelInput labelTitle={data.labelTitle}
+                                     labelText={data.labelText}
+                                     inputMultiLine={data.inputMultiLine}
+                                     rows = {data.rows}
+                                     maxLength = {data.maxLength}
+                                     fullWidth = {data.fullWidth}
+                                     onChange={(e)=>this.charCountChange(e)}
+                         />
+                    ))}
                     <RaisedButton label="Speichern" primary={true} style={style}/>
                 </Paper>
             </MuiThemeProvider>
